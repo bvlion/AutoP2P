9bfe683304ac64cd74aa8840ef9e9a116fcc6007
bvlion <iwai@wed.company>
2021-10-30 13:34:10 +0900

    first commit

---
+++ app/src/main/java/net/ambitious/android/autop2p/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package net.ambitious.android.autop2p.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Red200 = Color(0xFFFF867A)
+val Red500 = Color(0xFFEF534E)
+val Red700 = Color(0xFFB61825)
+val Teal200 = Color(0xFF03DAC5)
